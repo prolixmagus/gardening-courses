42234b765154634641b3b34fc0bbd6fa92900ef8
prolixmagus <jeremypfishman@gmail.com>
2024-10-16 16:07:37 -0700

    Add reset and set up plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [
+		vue(),
+		eslint({
+			include: ['src/**/*.js', 'src/**/*.vue'], // Adjust this to include your desired file types
+			exclude: ['node_modules/**'], // Exclude node_modules
+			// Optional: You can add additional ESLint options here if needed
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
